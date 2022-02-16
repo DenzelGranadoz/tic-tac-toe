a1517f88355ea758f678da2d411a2201f6ddbcdf
Denzel Granadoz <zelgranadoz@gmail.com>
2022-02-16 16:25:01 +0800

    added tie logic

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const DomElement = (() => {
   const startScreen = document.getElementById('start-screen');
   const startScreenForm = document.getElementById('start-screen-form');
   const labelPlayer2 = startScreenForm.getElementsByTagName('label')[1];
-  const player1 = document.getElementById('player1-name');
-  const player2 = document.getElementById('player2-name');
+  const namePlayer1 = document.getElementById('player1-name');
+  const namePlayer2 = document.getElementById('player2-name');
   const difficultyContainer = document.getElementById('difficulty-container');
   const difficulty = document.getElementById('difficulty');
   const playHuman = document.getElementById('human');
@@ -30,8 +30,8 @@ const DomElement = (() => {
     startScreen,
     startScreenForm,
     labelPlayer2,
-    player1,
-    player2,
+    namePlayer1,
+    namePlayer2,
     difficultyContainer,
     difficulty,
     playHuman,
@@ -75,6 +75,7 @@ const MainMenu = (() => {
   const showGameScreen = () => {
     DomElement.toggleNone(DomElement.startScreen);
     gameBoard.addBoardListeners();  
+    displayController.updateNameBoard();
   };
 
   DomElement.playButton.addEventListener('click', showGameScreen);
@@ -140,7 +141,6 @@ const Player = (sign, name) => {
 //gameboard logic
 const gameBoard = (() => {
   const board = new Array(9);
-
   const winCondition = [
     [0,1,2],
     [3,4,5],
@@ -179,51 +179,23 @@ const gameBoard = (() => {
         if(x_count === 3) {
           playerMark = 'X';
           winningCombination = i;
-          gameResultDeclare(winningCombination);
+          displayController.gameResult(winningCombination);
         } else if(o_count === 3) {
           playerWinner = 'O';
           winningCombination = i;
-          gameResultDeclare(winningCombination);
+          displayController.gameResult(winningCombination);
         }
       }
+    }
+
+    if(x_array.length === 5 && o_array.length === 4) {
+      displayController.deactivateBoard();
     }
     return {
       playerMark,
     }
   };
 
-  //will be moved to UI factory
-  const gameResultDeclare = (winningCombination) => {
-    removeBoardListeners();
-    removeHover();
-    highlightWinCombination(winningCombination);
-  };
-
-  const highlightWinCombination = (winningCombination) => {
-    winCombo = winCondition[winningCombination];
-    DomElement.boardCells.forEach((cell) => {
-      for(let i = 0; i < winCombo.length; i++) {
-        if(cell.id == winCombo[i]) {
-          cell.style.backgroundColor = 'limegreen';
-        }
-      }
-    });
-    //if id match with indexes with the element in an array, highlight block
-  };
-
-  //this removes hover properties when someone wins
-  const removeHover = () => {
-    DomElement.boardCells.forEach((cell) => {
-      if(cell.classList.contains('taken-cell')) {
-        cell.classList.remove('taken-cell');
-      }
-      cell.style.setProperty('opacity', 100); 
-    });
-  };
-
-  //function that returns the winning combination
-
-  //this function will be moved to display UI controller
   let boardMark = 'O';
   const updateMark = (e) => {
     if(boardMark === 'O') {
@@ -263,6 +235,65 @@ const gameBoard = (() => {
   };
 
   return {
-    addBoardListeners
+    winCondition,
+    addBoardListeners,
+    removeBoardListeners
+  }
+})();
+
+
+//to dow 
+//add draw
+//player name display
+//current player highlight
+//show reset and main menu button when round is over
+//round win race to 5 logic
+//add ai and its events
+const displayController = (() => {
+  const gameResult = (winningCombination) => {
+    deactivateBoard();
+    highlightWinCombination(winningCombination);
+  };
+
+  const deactivateBoard = () => {
+    gameBoard.removeBoardListeners();
+    removeHover();
+  };
+
+  const highlightWinCombination = (winningCombination) => {
+    let winCombo = gameBoard.winCondition[winningCombination];
+    DomElement.boardCells.forEach((cell) => {
+      for(let i = 0; i < winCombo.length; i++) {
+        if(cell.id == winCombo[i]) {
+          cell.style.backgroundColor = 'limegreen';
+        }
+      }
+    });
+  };
+
+  //this removes hover properties when someone wins
+  const removeHover = () => {
+    DomElement.boardCells.forEach((cell) => {
+      if(cell.classList.contains('taken-cell')) {
+        cell.classList.remove('taken-cell');
+      }
+      cell.style.setProperty('opacity', 100); 
+    });
+  };
+
+  const updateNameBoard = () => {
+    let score = 5;
+    if(DomElement.namePlayer1.value !== '') {
+      DomElement.nameScorePlayer1.innerHTML = `${DomElement.namePlayer1.value}: ${score}`;
+    }
+    if(DomElement.namePlayer2.value !== '') {
+      DomElement.nameScorePlayer2.innerHTML = `${DomElement.namePlayer2.value}: ${score}`;
+    }
+  };
+
+  return {
+    gameResult,
+    deactivateBoard,
+    updateNameBoard
   }
 })();
